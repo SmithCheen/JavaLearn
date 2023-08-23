176aa0844207d4c74de9d3e40881c0ecb644a54a
win <345514840@qq.com>
2023-08-23 19:40:13 +0800

    win test

--- src/chapter04/Java04_Polymorphic_2.java
+++ src/chapter04/Java04_Polymorphic_2.java
@@ -3,21 +3,23 @@ package chapter04;
 public class Java04_Polymorphic_2 {
     public static void main(String[] args) {
         // 给一个有普通收入、工资收入和享受国务院特殊津贴的小伙伴算税:
-        Income[] incomes = new Income[] {
+        Income[] incomes = new Income[]{
                 new Income(3000),
                 new Salary(7500),
                 new StateCouncilSpecialAllowance(15000)
         };
         System.out.println(totalTax(incomes));
     }
+
     public static double totalTax(Income... incomes) {
         double total = 0;
-        for (Income income: incomes) {
+        for (Income income : incomes) {
             total = total + income.getTax();
         }
         return total;
     }
 }
+
 class Income {
     protected double income;
 
