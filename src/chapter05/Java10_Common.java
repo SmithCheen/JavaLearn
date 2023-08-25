abf1d0e16c83301c72a04f24554287b055a0972e
kk <liu345514840@163.com>
2023-08-25 14:26:20 +0800

    BigInteger&BigDecimal&Common

--- src/chapter05/Java10_Common.java
+++ src/chapter05/Java10_Common.java
@@ -1,6 +1,22 @@
 package chapter05;
 
 public class Java10_Common {
-    // TODO record Java14新特性
-    // 与Lombok区分
+    public static void main(String[] args) {
+        // TODO Math
+        Math.abs(-100); // 100
+        Math.max(100, 99); // 100
+        Math.pow(2, 10);
+        Math.sqrt(2);
+        Math.exp(2);
+        Math.log(4);
+        Math.log10(100); // 2
+
+        // TODO HexFormat 进制转换十六进制
+
+
+        // TODO Random 伪随机数
+
+        // TODO SecureRandom 真随机数
+
+    }
 }
